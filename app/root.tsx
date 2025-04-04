8fa79c1003d7c8c0107f4a9e6a01b4e51110ea90
Stefan Huckschlag <stefan.huckschlag@web.de>
2025-04-04 08:42:18 +0200

    feat(root): add config script

--- app/root.tsx
+++ app/root.tsx
@@ -31,6 +31,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
+        <script src="/config.js"></script>
       </head>
       <body className="h-screen">
         {children}
