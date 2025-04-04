240ffd017a55435ef381c37eb7b23a5d88565383
Stefan Huckschlag <stefan.huckschlag@web.de>
2025-04-04 08:46:43 +0200

    style(link): change colors

--- app/navigation/link.tsx
+++ app/navigation/link.tsx
@@ -14,13 +14,13 @@ export const Link = ({ to, className, children, ...rest }: LinkProps) => {
     <NavLink
       className={({ isActive, isPending, isTransitioning }) => {
         const color = isActive
-          ? "bg-orange-200 dark:bg-orange-700 hover:bg-orange-300 dark:hover:bg-orange-600 focus:ring-orange-500"
+          ? "bg-orange-200 dark:bg-orange-600 hover:bg-orange-300 dark:hover:bg-orange-500 focus:ring-orange-500"
           : isPending
-          ? "bg-blue-100 dark:bg-blue-900 hover:bg-blue-200 dark:hover:bg-blue-800 focus:ring-blue-500"
+          ? "bg-blue-100 dark:bg-blue-700 hover:bg-blue-200 dark:hover:bg-blue-600 focus:ring-blue-500"
           : isTransitioning
-          ? "bg-blue-100 dark:bg-blue-900 hover:bg-blue-200 dark:hover:bg-blue-800 focus:ring-blue-500"
-          : "bg-blue-100 dark:bg-blue-900 hover:bg-blue-200 dark:hover:bg-blue-800 focus:ring-blue-500";
-        return `grid grid-cols-[1fr_2fr] px-2 py-1 m-1 text-xs items-center  rounded ${color} focus:outline-none focus:ring-2  focus:ring-opacity-50 ${className}`;
+          ? "bg-blue-100 dark:bg-blue-700 hover:bg-blue-200 dark:hover:bg-blue-600 focus:ring-blue-500"
+          : "bg-blue-100 dark:bg-blue-700 hover:bg-blue-200 dark:hover:bg-blue-600 focus:ring-blue-500";
+        return `grid grid-cols-[1fr_2fr] px-2 py-1 m-1 text-xs items-center rounded ${color} focus:outline-none focus:ring-2 focus:ring-opacity-50 ${className}`;
       }}
       to={to}
       {...rest}
