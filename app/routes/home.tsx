9bc4d0c80784b0e643682b3286bbd9304576c041
Stefan Huckschlag <stefan.huckschlag@web.de>
2025-04-04 08:44:16 +0200

    feat(home): direct focus to nav

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,12 +1,31 @@
+import { ArrowLeftCircleIcon } from "@heroicons/react/16/solid";
 import type { Route } from "./+types/home";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Mundwerk Telefonzentrale" },
+    { name: "description", content: "Hier ist die Zentrale des Telefons. ;-)" },
   ];
 }
 
 export default function Home() {
-  return <div>Home</div>;
+  // we want to point the users to use the left side navigation
+
+  return (
+    <div className="bg-slate-100 dark:bg-slate-700 h-full w-full pt-4">
+      <h1 className="text-4xl font-bold text-center">
+        Mundwerk Telefonzentrale
+      </h1>
+      <p className="text-lg mt-4 text-center">
+        Hier ist die Zentrale des Telefons. ;-)
+      </p>
+      <p className="text-lg mt-4 text-center">
+        <ArrowLeftCircleIcon className="h-6 w-6 text-blue-500 dark:text-blue-300 inline-block" />{" "}
+        <span className="hidden md:inline">
+          Bitte nutze die Navigation links, um auf die verschiedenen Bereiche
+          zuzugreifen.
+        </span>
+      </p>
+    </div>
+  );
 }
