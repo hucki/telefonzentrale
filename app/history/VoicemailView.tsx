eb336160d17fc408a99cf355e48a9703e5ba2703
Stefan Huckschlag <stefan.huckschlag@web.de>
2025-04-04 08:47:12 +0200

    feat(VoicemailView): new component

---
+++ app/history/VoicemailView.tsx
@@ -0,0 +1,23 @@
+import type { HistoryItem } from "../types/history";
+import HistoryItemListContainer from "./HistoryItemListContainer";
+
+interface HistoryViewProps {
+  voicemails: { items: HistoryItem[] };
+  isArchive?: boolean;
+}
+export const VoicemailView = ({
+  voicemails,
+  isArchive = false,
+}: HistoryViewProps) => {
+  return (
+    <div className="grid grid-cols-1 h-full w-full p-2 overflow-y-auto">
+      <HistoryItemListContainer
+        items={voicemails.items}
+        isArchive={isArchive}
+        type="VOICEMAIL"
+        direction="INCOMING"
+        className="md:row-span-2"
+      />
+    </div>
+  );
+};
