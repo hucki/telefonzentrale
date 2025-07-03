6157a1e9dd2b191a4c33f3ab921276a4010b205d
Stefan Huckschlag <stefan.huckschlag@web.de>
2025-07-03 10:24:56 +0200

    feat(ResendFaxButton): new component

---
+++ app/components/resendFaxButton.tsx
@@ -0,0 +1,21 @@
+import { useFetcher } from "react-router";
+import { SimpleButton } from "~/components/buttons";
+/**
+ * Button to resend a fax.
+ * This component is used in the FaxHistoryItemList to allow users to resend a fax.
+ *
+ * @param {Object} props - The component props.
+ * @param {string} props.faxId - The ID of the fax to resend.
+ */
+export const ResendFaxButton = ({ faxId }: { faxId: string }) => {
+  const fetcher = useFetcher();
+  const pending = fetcher.state !== "idle";
+
+  return (
+    <fetcher.Form method="POST" action="/fax">
+      <input type="hidden" name="actionType" value="resendFax" />
+      <input type="hidden" name="faxId" value={faxId} />
+      <SimpleButton label="erneut senden" type="submit" pending={pending} />
+    </fetcher.Form>
+  );
+};
