dfee8e1b0880c623737933cfef4aae26980085b7
Stefan Huckschlag <stefan.huckschlag@web.de>
2025-10-16 17:15:39 +0200

    feat(root): add favicon link

--- app/root.tsx
+++ app/root.tsx
@@ -29,6 +29,9 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" sizes="32x32" />
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+        <link rel="shortcut icon" href="/favicon.ico" />
         <Meta />
         <Links />
         <script src="/config.js"></script>
